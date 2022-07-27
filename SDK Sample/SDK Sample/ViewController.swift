5ed24acf892a34ab3b5091966edaa0c410bfb267
Manel Roca <manel.roca@meetinglawyers.com>
2022-07-27 11:00:42 +0200

    initial commit

---
+++ SDK Sample/SDK Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SDK Sample
+//
+//  Created by Manel MeetingLawyers on 27/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
