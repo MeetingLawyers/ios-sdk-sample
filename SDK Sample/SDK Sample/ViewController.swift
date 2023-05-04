f450e641ce3ad98a1aa0aec84e9eee976196c8d8
Manel Roca <manel.roca@meetinglawyers.com>
2023-05-04 17:28:02 +0200

    feat: update to new iOS SDK

--- SDK Sample/SDK Sample/ViewController.swift
+++ SDK Sample/SDK Sample/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MeetingLawyers
 
 class ViewController: UIViewController {
 
@@ -14,6 +15,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onClickButton(_ sender: Any) {
+        self.configStyle()
+        self.authenticate()
+    }
 }
 
+extension ViewController {
+    private func configStyle() {
+        MeetingLawyersApp.setStyle(primaryColor: .blue)
+        MeetingLawyersApp.setStyle(secondaryColor: .systemBlue)
+    }
+
+    private func authenticate() {
+        let userId = "<#userID#>"
+        MeetingLawyersApp.authenticate(userId: userId) { error in
+            guard let error = error else {
+                // AUTH OK
+                self.launchProfessionalList()
+                return
+            }
+            // AUTH KO
+        }
+    }
+
+    private func launchProfessionalList() {
+        if let result = MeetingLawyersApp.messengerViewController() {
+            present(result, animated: true)
+        }
+    }
+}
