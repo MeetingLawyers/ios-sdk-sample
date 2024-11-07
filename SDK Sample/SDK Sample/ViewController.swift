c47892955f45d9020d69b3ed0f42365c60305811
Joseps <jose.pose@meetinglaywers.com>
2024-11-07 15:28:59 +0100

    feat: Update sample to 2.0.0-beta06
    Add more options and styles

--- SDK Sample/SDK Sample/ViewController.swift
+++ SDK Sample/SDK Sample/ViewController.swift
@@ -24,6 +24,8 @@ extension ViewController {
     private func configStyle() {
         MeetingLawyersApp.setStyle(primaryColor: .blue)
         MeetingLawyersApp.setStyle(secondaryColor: .systemBlue)
+        MeetingLawyersApp.setStyle(navigationTitleView: getNavigationLogoView())
+        MeetingLawyersApp.setStyle(navigationLeftBarButtons: getNavigationLeftBarButtons())
     }
 
     private func configMoreSpecificStyles() {
@@ -37,7 +39,8 @@ extension ViewController {
     private func launchSdk() {
         Task {
             do {
-                try await MeetingLawyersApp.configure(apiKey: Constants.apiKey)
+                let configuration = MeetingLawyersConfiguration(apiKey: Constants.apiKey, environment: RemoteEnvironment.development, logType: .debug)
+                try await MeetingLawyersApp.configure(configuration)
                 try await MeetingLawyersApp.authenticate(userId: Constants.userId)
                 self.launchProfessionalList()
             } catch {
@@ -53,5 +56,40 @@ extension ViewController {
                 self.present(result, animated: true)
             }
         }
+    }
+}
+
+extension ViewController {
+
+    private func getNavigationLogoView() -> UIView {
+        let titleView = UIView(frame: CGRect.zero)
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "heart.fill")
+        imageView.contentMode = .scaleAspectFit
+
+        titleView.addSubview(imageView)
+
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 150),
+            imageView.heightAnchor.constraint(equalToConstant: 50),
+            imageView.centerXAnchor.constraint(equalTo: titleView.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: titleView.centerYAnchor)
+        ])
+        return titleView
+    }
+
+    private func getNavigationLeftBarButtons() -> [UIBarButtonItem] {
+        // Add top button bar
+        let rightButton = UIBarButtonItem(image: UIImage(systemName: "heart.fill"),
+                                              style: .plain,
+                                              target: self,
+                                              action: #selector(self.onClickNavigationRightButton))
+        return [rightButton]
+    }
+    @objc func onClickNavigationRightButton() {
+        print("onClickNavigationRightButton")
     }
 }
